a33fe050ab5de6854023531d6e7d8fd4b27e1a89
BillSnook <billsnook@mac.com>
2016-02-25 17:12:05 -0800

    Initial Commit

---
+++ totd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  totd
+//
+//  Created by Bill Snook on 2/25/16.
+//  Copyright © 2016 BillSnook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
